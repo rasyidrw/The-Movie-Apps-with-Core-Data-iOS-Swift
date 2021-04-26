4250849bfac51a221269f6aefe79d6648f0c16c9
Rasyid <rasyidrespati.30@gmail.com>
2021-04-26 17:07:56 +0700

    set launch screen delay

--- The Movie Apps with Core Data - iOS Swift/Popular Movies/Popular Movies/AppDelegate.swift
+++ The Movie Apps with Core Data - iOS Swift/Popular Movies/Popular Movies/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIStoryboard(name: "Launch Screen", bundle: nil).instantiateInitialViewController()
+        window?.makeKeyAndVisible()
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
+            self.window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+        }
+        
         return true
     }
 
